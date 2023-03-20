51d723bbed9d0f8d5416205533e70e10e5e16ecd
CeMaGo <119850565+CeMaGo@users.noreply.github.com>
2023-03-20 18:40:31 +0100

    not pretty, but at least NextAuth GitHub is working... now polishing up and fixing the Credential signup

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,10 @@
 import "@/styles/globals.css";
 import { SessionProvider } from "next-auth/react";
 
-export default function App({ Component, pageProps, session }) {
-  //   pageProps: { session, ...pageProps },
-  // })
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
   return (
     <SessionProvider session={pageProps.session}>
       <Component {...pageProps} />;
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,6 +1,6 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
-// import { CredentialsProvider } from "next-auth/providers";
+// import CredentialProvider from "next-auth/providers";
 import connectMongo from "@/database/connectDb";
 import { compare } from "bcryptjs";
 import Users from "@/model/schema";
@@ -8,35 +8,36 @@ import Users from "@/model/schema";
 export default NextAuth({
   providers: [
     GithubProvider({
-      clientId: process.env.GITHUB_CLIENT_ID,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    }),
-    CredentialsProvider({
-      name: "Credentials",
-      async authorize(credentials, req) {
-        connectMongo().catch((error) => {
-          error: "Connection Failed...!";
-        });
-
-        // check if user is already existing
-        const result = await Users.findOne({ email: credentials.email });
-        if (!result) {
-          throw new Error(
-            " A User with the given Email does not exist, please Sign Up!"
-          );
-        } // User does exist, now compare passwords..
-        const checkPassword = await compare(
-          credentials.password,
-          result.password
-        );
-        //incorrect?
-        if (!checkPassword || result.email !== credentials.email) {
-          throw new Error("Username or Password is not matching");
-        }
-        return result;
-      },
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
     }),
   ],
+  //   CredentialProviders({
+  //     name: "Credentials",
+  //     async authorize(credentials, req) {
+  //       connectMongo().catch((error) => {
+  //         error: "Connection Failed...!";
+  //       });
+
+  //       // check if user is already existing
+  //       const result = await Users.findOne({ email: credentials.email });
+  //       if (!result) {
+  //         throw new Error(
+  //           " A User with the given Email does not exist, please Sign Up!"
+  //         );
+  //       } // User does exist, now compare passwords..
+  //       const checkPassword = await compare(
+  //         credentials.password,
+  //         result.password
+  //       );
+  //       //incorrect?
+  //       if (!checkPassword || result.email !== credentials.email) {
+  //         throw new Error("Username or Password is not matching");
+  //       }
+  //       return result;
+  //     },
+  //   }),
+  // ],
   // node <openssl rand -base64 32> / jwt.io allows one to decoded the Web token
   secret: process.env.JWT_SECRET,
   session: {
