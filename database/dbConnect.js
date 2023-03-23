3a1d7e43ef918a93977930c4ae7766b26936a83f
CeMaGo <119850565+CeMaGo@users.noreply.github.com>
2023-03-23 08:16:32 +0100

    something is happening, at least getting a 404 error...next round error hunting

--- database/dbConnect.js
+++ database/dbConnect.js
@@ -2,9 +2,17 @@ import mongoose from "mongoose";
 
 const connectMongo = async () => {
   try {
-    const { connection } = await mongoose.connect(process.env.MONGODB_URL);
+    const userMongo = process.env.MONGO_USER;
+    const passwordMongo = process.env.MONGO_PW;
+    const databaseMongo = process.env.MONGO_DB;
+    const connectionString = `mongodb=srv:??${userMongo}:${passwordMongo}@mongotemplatecluster.hrx7eky.mongodb.net/${databaseMongo}?retryWrites=true&w=majority`;
+    const { connection } = await mongoose.connect(connectionString, {
+      userNewUrlParser: true,
+      useUnifiedTopology: true,
+      autoIndex: true,
+    });
     if (connection.readyState.State == 1) {
-      return Promise.resolve(true);
+      return await Promise.resolve(true);
     }
   } catch (error) {
     return Promise.reject(error);
