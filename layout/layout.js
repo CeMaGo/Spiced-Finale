38253b82731f0d0fb9eae2b50c1c1f971f1373cb
CeMaGo <119850565+CeMaGo@users.noreply.github.com>
2023-03-28 18:12:06 +0200

    added a little styling to the audiotag

--- layout/layout.js
+++ layout/layout.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Layout({ children }) {
   return (
-    <div className="flex h-screen bg-gradient-to-r from-sky-700 to-slate-800">
+    <div className="flex vh-screen bg-gradient-to-r from-sky-700 to-slate-800">
       <div className="m-auto bg-sky-50 rounded-md w-4/5 h-4/4 grid lg:grid-cols-2">
         <div className="right flex flex-col justify-evenly">
           <div className="text-center py-8">{children}</div>
--- pages/player.js
+++ pages/player.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { useRouter } from "next/router";
 import { BiWater } from "react-icons/bi";
+import { BsFire } from "react-icons/bs";
+import { GiEarthAmerica } from "react-icons/gi";
+import { MdOutlineAir } from "react-icons/md";
 import { AudioPlayer } from "@/components/AudioPlayer";
 import MusicBubbles from "@/components/MusicBubbles/MusicBubbles";
 import ReactAudioPlayer from "react-audio-player";
@@ -16,29 +19,42 @@ function Player() {
   return (
     <div>
       <MusicBubbles />
+      Koshi
       <br />
       {/* <AudioPlayer src="../public/Sounds/Koshi/Aria.mp3" /> */}
-      <div
-      // onClick={() => {
-      //   console.log("koshi clicked"), source.play();
-      // }}
-      >
-        Play koshi
-        <title>🌎</title>
-        <audio controls src={"/Terra2.mp3"} title="🌎"></audio>
+      <div>
+        <div className="grid">
+          <GiEarthAmerica className=" mx-3 h-10 w-10 align-center justify-center text-amber-600 shadow rounded-full" />
+          <audio controls title="🌎" className="mx-2 shadow-lg rounded-full">
+            <source src={"/Terra2.mp3"} typeof="audio/mpeg" />
+          </audio>
+        </div>
         <br />
         <div>
-          <audio title="💧Water" controls tabindex="0">
+          <BiWater className=" mx-3 h-10 w-10 align-center justify-center text-sky-500 shadow rounded-full" />
+          <audio
+            title="💧Water"
+            controls
+            tabindex="0"
+            className="mx-2 shadow-lg rounded-full"
+          >
             <source src={"/Auqua.mp3"} type="audio/mpeg" />
           </audio>
         </div>
         <br />
-        <audio autoPlay controls="controls">
+        <BsFire className=" mx-3 h-10 w-10 align-center justify-center text-red-400 shadow rounded-full" />
+        <audio controls className="mx-2 shadow-lg rounded-full">
           <source src={"/Ignis.mp3"} type="audio/mpeg" />
-          Your browser does not support the audio tag.
         </audio>
+        <br />
       </div>
-      <ReactAudioPlayer src={"/Aria.mp3"} autoPlay controls type="audio/mpeg" />
+      <MdOutlineAir className=" mx-3 h-10 w-10 align-center justify-center text-pink-200 shadow rounded-full" />
+      <audio
+        src={"/Aria.mp3"}
+        controls
+        type="audio/mpeg"
+        className="mx-2 shadow-lg rounded-full"
+      />
     </div>
   );
 }
